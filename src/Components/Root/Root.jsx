7ff79a8d1fb62a7026fbf37e4a6d3374e41935c3
joymajumder23 <joymajumderjishu@gmail.com>
2024-05-12 03:20:21 +0600

    navbar section added

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import Home from '../Home/Home';
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Root;
